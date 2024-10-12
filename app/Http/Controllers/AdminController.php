5a4343052285bdde882b105a91b9788d2cdb01e2
Forhad-Hossain <pycoder028@gmail.com>
2024-10-12 09:31:21 +0600

    Admin Template Setup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,7 +9,7 @@ class AdminController extends Controller
     
     public function AdminDashboard(Request $request){
 
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     }
 
 }
