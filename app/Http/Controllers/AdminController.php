becf9668a241828eb12d1d8b951e4d46486e345d
Forhad-Hossain <pycoder028@gmail.com>
2024-10-12 10:34:18 +0600

    Admin Logout Setup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class AdminController extends Controller
 {
@@ -10,6 +11,14 @@ class AdminController extends Controller
     public function AdminDashboard(Request $request){
 
         return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 
 }
